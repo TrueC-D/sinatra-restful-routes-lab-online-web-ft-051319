33886c533701c93288f77ac792ad643e37f97f7c
Alethea Cary <aletheacary@gmail.com>
2019-07-24 17:21:39 +0000

    Done.

--- db/migrate/20190724170946_create_recipes_table.rb
+++ db/migrate/20190724170946_create_recipes_table.rb
@@ -3,6 +3,6 @@ class CreateRecipesTable < ActiveRecord::Migration
     create_table :recipes |t|
     t.string :name
     t.text :ingredients
-    t.integer :cook_time
+    t.string :cook_time
   end
 end
